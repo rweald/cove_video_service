d3c1aaa2cf8bb8da8f0697281e08ac553317bfe5
Ryan Weald <ryan@weald.com>
2011-04-19 11:47:02 -0700

    a token is now being generated and returned to the user. TODO: Implement a token check on the video render method

--- simple_server.rb
+++ simple_server.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 require 'sinatra'
 require 'yajl/json_gem'
 require 'erb'
+require 'openssl'
 
 set :views, File.join(File.dirname(__FILE__), "templates")
 
@@ -16,10 +17,19 @@ get '/token' do
   unless params[:video]
     response.status = 400
     content_type :json
-    return {"status" => "No video specified you must specify a video"}.to_json
+    return {"status" => "failed", "notice" => "No video specified you must specify a video"}.to_json
   end
+  token = generate_token
+  {"status" => "success", 'token' => token}.to_json
 end
 
 get '/video' do
   send_file File.join(File.dirname(__FILE__), "videos", params[:name])
+end
+
+def ensure_video_param
+end
+
+def generate_token
+  OpenSSL::BN.rand(2*16).to_s(16)
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -16,8 +16,29 @@ describe "SimpleServer" do
       end
       it "should send failure message" do
         resp = JSON.parse(last_response.body)
-        resp['status'].should == "No video specified you must specify a video"
+        resp['status'].should == "failed"
+      end
+
+      it "should send a notice that specifies the problem" do
+        resp = JSON.parse(last_response.body)
+        resp['notice'].should == "No video specified you must specify a video"
       end
     end   
+
+    context "video specified" do
+      before(:each) do
+        resp = get '/token', :video => "videoplayback.webm"
+        @resp = JSON.parse(resp.body)
+      end
+      it "should return status 200" do
+        last_response.status.should == 200
+      end
+      it "should send success message in json" do
+        @resp['status'].should == 'success'
+      end
+      it "should return a token in the json" do
+        @resp['token'].should be
+      end
+    end
   end
 end
